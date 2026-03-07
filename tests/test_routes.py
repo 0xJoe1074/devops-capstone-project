1ef8b51d031f5516c72f0cb5adef05aa56436607
“0xJoe1074” <lindemann.joerg@gmail.com>
2026-03-07 13:31:51 -0500

    fix: repair indentation error in test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -23,7 +23,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
- class TestAccountService(TestCase):
+class TestAccountService(TestCase):
     """Account Service Tests"""
 
     @classmethod
